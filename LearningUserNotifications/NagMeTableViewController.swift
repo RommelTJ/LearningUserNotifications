cf5dabd263186fedab1fbdee0154c5a2dfde4fb0
Rommel Rico <me@rommelrico.com>
2017-10-26 08:36:21 -0700

    The rest of the NagMeTableViewController

--- LearningUserNotifications/NagMeTableViewController.swift
+++ LearningUserNotifications/NagMeTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class NagMeTableViewController: UITableViewController {
 
@@ -20,84 +21,120 @@ class NagMeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.allowsSelection = false
+        
+        // Reload the tableview when the app is reloaded... want to update the icon states in case the user has cancelled a notification
+        NotificationCenter.default.addObserver(tableView, selector: #selector(UITableView.reloadData), name: NSNotification.Name.UIApplicationDidBecomeActive, object:nil)
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCell", for: indexPath) as! TaskCell
+        
+        let task = tasks[indexPath.row]
+        
+        cell.nameLabel.text = task
+        
+        // Set the cell's icon to indicate whether notification exists or not
+        retrieveNotification(for: task) {
+            request in
+            request != nil ? cell.showReminderOnIcon() : cell.showReminderOffIcon()
+        }
+        
+        // Closure invoked when button tapped
+        cell.onButtonSelection = {
+            [unowned self] in
+            self.toggleReminder(for: task)
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    /// Returns the cell displaying the given task, if exists
+    func cell(for task: Task) -> TaskCell? {
+        let index = tasks.index(of: task)!
+        let path = IndexPath(row: index, section: 0)
+        return tableView.cellForRow(at: path) as? TaskCell
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    // MARK: - Notification Retrieval
+    
+    /// If a notification is scheduled for the given task, it's turned off and vice versa.
+    func toggleReminder(for task: Task) {
+        
+        retrieveNotification(for: task) {
+            request in
+            guard request != nil else {
+                // Notification doesn't exist, therefore schedule it
+                self.createReminderNotification(for: task)
+                return
+            }
+            
+            // Remove notification
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [task])
+            
+            // Now we've muted the notification, let's set the cell's icon to reflect that.
+            if let cell = self.cell(for: task) {
+                cell.showReminderOffIcon()
+            }
+        }
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    /// Creates a notification for the given task, repeated every minute.
+    func createReminderNotification(for task: Task) {
+        // Configure our notification's content
+        let content = UNMutableNotificationContent()
+        content.title = "Task Reminder"
+        content.body = "\(task)!!" // Important to include this, otherwise notification won't display
+        content.sound = UNNotificationSound.default()
+        content.categoryIdentifier = Identifiers.reminderCategory
+        
+        // We want the notification to nag us every 60 seconds (the minimum time-interval Apple allows us to repeat at)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        
+        // Simply use the task name as our identifier. This means we have a single notification per task... any other notifications created with same identifier will overwrite the existing notification.
+        let identifier = "\(task)"
+        
+        // Construct the request with the above components
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) {
+            error in
+            if let error = error {
+                print("Problem adding notification: \(error.localizedDescription)")
+            }
+            else {
+                // Set icon
+                DispatchQueue.main.async {
+                    if let cell = self.cell(for: task) {
+                        cell.showReminderOnIcon()
+                    }
+                }
+            }
+        }
     }
-    */
+    
+    /// Attempts to find the notification scheduled for the given task name.
+    func retrieveNotification(for task: Task, completion: @escaping (UNNotificationRequest?) -> ()) {
+        UNUserNotificationCenter.current().getPendingNotificationRequests {
+            requests in
+            // The pending requests are fetched on a background thread, so need to ensure we access our properties and UI elements on main thread
+            DispatchQueue.main.async {
+                let request = requests.filter { $0.identifier == task }.first
+                completion(request)
+            }
+        }
+    }
 
 }
