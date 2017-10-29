975400f31445810635071d2e6a70fd3ead01ba5c
Rommel Rico <me@rommelrico.com>
2017-10-28 20:45:36 -0700

    Creating the custom UI

--- LearningUserNotificationsContentExtension/NotificationViewController.swift
+++ LearningUserNotificationsContentExtension/NotificationViewController.swift
@@ -12,7 +12,8 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
-    @IBOutlet var label: UILabel?
+    @IBOutlet var label: UILabel!
+    @IBOutlet weak var speakerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +21,51 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     }
     
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = notification.request.content.body
+        label.text = "Reminder: \(notification.request.content.body)"
+        speakerLabel.shake()
+    }
+    
+    func didReceive(_ response: UNNotificationResponse,
+                    completionHandler completion: @escaping (UNNotificationContentExtensionResponseOption) -> Void) {
+        
+        if response.actionIdentifier == Identifiers.cancelAction {
+            let request = response.notification.request
+            
+            let identifiers = [request.identifier]
+            
+            // Remove future notifications that have been scheduled
+            UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: identifiers)
+            
+            // Remove any notifications that have already been delivered so we're not cluttering up the user's notification center
+            UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: identifiers)
+            
+            // Visual feedback that notification has been cancelled
+            speakerLabel.text = "🔇"
+            speakerLabel.cancelShake()
+            
+            completion(.doNotDismiss)
+        }
+        else {
+            completion(.dismiss)
+        }
     }
 
 }
+
+extension UIView {
+    
+    // Very slightly modified version of sample code found in stack overflow post http://stackoverflow.com/a/34778432
+    func shake() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = 1
+        animation.repeatCount = .infinity
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        layer.add(animation, forKey: "shake")
+    }
+    
+    func cancelShake() {
+        layer.removeAnimation(forKey: "shake")
+    }
+}
+
